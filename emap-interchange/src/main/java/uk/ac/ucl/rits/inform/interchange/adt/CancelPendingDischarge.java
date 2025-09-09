eb11492b89186d295fc184494b360dd71f253be4
Stephen Thompson <s.thompson@ucl.ac.uk>
2025-09-09 09:11:14 +0100

    Added interchange message definitions

---
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/CancelPendingDischarge.java
@@ -0,0 +1,34 @@
+package uk.ac.ucl.rits.inform.interchange.adt;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
+import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessor;
+import uk.ac.ucl.rits.inform.interchange.InterchangeValue;
+
+import java.time.Instant;
+
+/**
+ * Cancel pending Discharge event.
+ * Implemented: ADT A25 (discharge)
+ * Not implemented: ADT A27 (admit)
+ * @author Steve Thompson
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class CancelPendingDischarge extends AdtMessage implements PendingEvent, AdtCancellation {
+    private PendingType pendingEventType = PendingType.DISCHARGE;
+    private String dischargeDisposition;
+    private String dischargeLocation;
+    private InterchangeValue<String> hospitalService = InterchangeValue.unknown();
+    private InterchangeValue<String> pendingDestination = InterchangeValue.unknown();
+    private Instant cancelledDateTime;
+
+
+    @Override
+    public void processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
+        processor.processMessage(this);
+    }
+}
---
+++ emap-interchange/src/main/java/uk/ac/ucl/rits/inform/interchange/adt/PendingDischarge.java
@@ -0,0 +1,32 @@
+package uk.ac.ucl.rits.inform.interchange.adt;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessingException;
+import uk.ac.ucl.rits.inform.interchange.EmapOperationMessageProcessor;
+import uk.ac.ucl.rits.inform.interchange.InterchangeValue;
+
+import java.time.Instant;
+/**
+ * Pending Dishcharge event.
+ * Implemented: ADT A16 (Pending Discharge)
+ * Not implemented: ADT A14 (Pending admit)
+ * @author Steve Thompson
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class PendingDischarge extends AdtMessage implements PendingEvent{
+    private PendingType pendingEventType = PendingType.DISCHARGE;
+    private InterchangeValue<String> pendingDestination = InterchangeValue.unknown();
+    private InterchangeValue<String> hospitalService = InterchangeValue.unknown();
+    private String dischargeDisposition;
+    private String dischargeLocation;
+    private Instant dischargeDateTime;
+
+    @Override
+    public void processMessage(EmapOperationMessageProcessor processor) throws EmapOperationMessageProcessingException {
+        processor.processMessage(this);
+    }
+}
